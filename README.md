3fc2d8515fc4ba58cbbc4b9ed6d098133a4ffa06
filipe.dias <fedispato@gmail.com>
2024-03-24 15:34:46 -0300

    feat: Adicionando código do spring security com filterChain

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Spring Security
 - Projeto com configuração do Spring Security
+
+### Links e referências
+- Spring Security without the WebSecurityConfigurerAdapter
+  - https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
--- src/main/java/tortora/spring/security/WebSecurityConfig.java
+++ src/main/java/tortora/spring/security/WebSecurityConfig.java
@@ -3,13 +3,32 @@ package tortora.spring.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests( (authorize) -> authorize
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/managers").hasRole("MANAGERS")
+                .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
